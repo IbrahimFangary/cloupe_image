15cc73584b635c348a836ff5fe19438ea44948e4
IbrahimFangary <165485954+IbrahimFangary@users.noreply.github.com>
2025-05-27 04:34:30 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cloupe_image",
+    version="0.1.0",
+    author="Ibrahim Fangary",
+    description="Tool to stitch high-res images (the closest resolution to the microscope image) from 10x Genomics .cloupe files",
+    packages=find_packages(),
+    install_requires=[
+        "Pillow",
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'stitch-cloupe=cloupe_image.stitch_cloupe_image:stitch_cloupe_image_cli',
+        ],
+    },
+)
+
