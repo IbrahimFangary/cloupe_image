a8711422da718e74cd5815233b8b6df5709727e9
IbrahimFangary <165485954+IbrahimFangary@users.noreply.github.com>
2025-05-27 04:32:49 +0300

    Update __init__.py

--- stitch_cloupe_image/__init__.py
+++ stitch_cloupe_image/__init__.py
@@ -4,4 +4,6 @@ cloupe_image_stitcher
 A package to extract and stitch spatial transcriptomics images from 10x Genomics .cloupe files.
 """
 
-from .stitch import stitch_cloupe_image  # assuming you refactor main function as stitch_cloupe_image()
+from .stitch_cloupe_image import stitch_cloupe_image
+
+__all__ = ["stitch_cloupe_image"]
